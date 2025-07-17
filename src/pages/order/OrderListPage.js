1419638c31892e0d036b5136159ed5056e143f35
petertecnetdev <petertecnet@gmail.com>
2025-07-16 22:27:15 -0300

    item

--- src/pages/order/OrderListPage.js
+++ src/pages/order/OrderListPage.js
@@ -198,7 +198,7 @@ const handleReprint = async (orderId) => {
     padding: 0;
     width: 80mm;
     font-family: monospace;
-    font-size: 16px;
+    font-size: 18px;
     line-height: 2.4;
   }
   pre { white-space: pre-wrap; word-wrap: break-word; }
