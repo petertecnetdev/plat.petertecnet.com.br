45fa02fa00d3b1862f2ee4f083d6104da6930e50
Peter Tecnet <petertecnet@gmail.com>
2026-08-28 21:21:42 -0300

    feat: show Peter Tecnet signature globally

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import PeterTecnetSignature from "./components/PeterTecnetSignature";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./LegacyTheme.css";
 import "./pages/establishment/Management.css";
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
       <App />
+      <PeterTecnetSignature />
     </GoogleOAuthProvider>
   </React.StrictMode>
 );
