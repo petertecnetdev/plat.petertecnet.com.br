dd297f707382a609ac0dd28b916cdaf4fd8d0c27
Peter Tecnet <petertecnet@gmail.com>
2026-08-29 14:44:30 -0300

    Sign direct Plat axios requests

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import axios from "axios";
 import App from "./App";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import PeterTecnetSignature from "./components/PeterTecnetSignature";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./LegacyTheme.css";
 import "./pages/establishment/Management.css";
+
+axios.defaults.headers.common["X-Peter-App"] = "plat";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
