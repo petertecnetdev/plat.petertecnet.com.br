5d2afa7cb43337bb35296cc88e34066d93451a0c
Peter Tecnet <petertecnet@gmail.com>
2026-08-29 14:43:36 -0300

    Identify Plat in every API request

--- src/services/api.js
+++ src/services/api.js
@@ -1,17 +1,31 @@
 // src/services/api.js
 import axios from "axios";
 
+const APP_SLUG = "plat";
+
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,  // deve ser "https://api.petertecnet.com.br/api"
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
-    "Content-Type": "application/json",
+    Accept: "application/json",
+    "X-Peter-App": APP_SLUG,
   },
+  timeout: 20000,
 });
 
-// opcional: injeta o Bearer token em todas as requisições
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
+  config.headers = config.headers || {};
+  config.headers["X-Peter-App"] = APP_SLUG;
+
   if (token) config.headers.Authorization = `Bearer ${token}`;
+
+  if (typeof FormData !== "undefined" && config.data instanceof FormData) {
+    delete config.headers["Content-Type"];
+    delete config.headers["content-type"];
+  } else if (!config.headers["Content-Type"] && !config.headers["content-type"]) {
+    config.headers["Content-Type"] = "application/json";
+  }
+
   return config;
 });
 
