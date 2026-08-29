9a7bc61d22448c6b9496d7aa8b04b85766589856
Peter Tecnet <petertecnet@gmail.com>
2026-08-28 21:19:42 -0300

    Load Plat establishment management styles globally

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./pages/establishment/Management.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
