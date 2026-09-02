2db132660188d801de3afed35d1ac7c966d3f555
Peter Tecnet <petertecnet@gmail.com>
2026-09-02 00:54:27 -0300

    feat(reliability): load final polish and error boundary

--- src/index.js
+++ src/index.js
@@ -7,26 +7,31 @@ import App from "./App";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import PeterTecnetSignature from "./components/PeterTecnetSignature";
 import PeterAccountGateway from "./components/PeterAccountGateway";
+import AppErrorBoundary from "./components/AppErrorBoundary";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./LegacyTheme.css";
 import "./pages/establishment/Management.css";
-import "./Responsive.css";
 import "./PeterAppTheme.css";
 import "./PlatLayoutSystem.css";
 import "./PlatPolish.css";
+import "./Responsive.css";
+import "./ProductionReady.css";
 
 startTelemetry({ apiBaseUrl, appSlug, appId });
 
 axios.defaults.headers.common["X-Peter-App"] = appSlug;
 axios.defaults.headers.common["X-App-ID"] = String(appId);
+axios.defaults.timeout = 30000;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <PeterAccountGateway apiBaseUrl={apiBaseUrl} appSlug={appSlug}>
-      <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
-        <App />
-        <PeterTecnetSignature />
-      </GoogleOAuthProvider>
-    </PeterAccountGateway>
+    <AppErrorBoundary>
+      <PeterAccountGateway apiBaseUrl={apiBaseUrl} appSlug={appSlug}>
+        <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+          <App />
+          <PeterTecnetSignature />
+        </GoogleOAuthProvider>
+      </PeterAccountGateway>
+    </AppErrorBoundary>
   </React.StrictMode>
 );
