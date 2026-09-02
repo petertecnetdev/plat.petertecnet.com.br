ebb49df6eb642486fa232cc78150ad4633678375
Peter Tecnet <petertecnet@gmail.com>
2026-09-01 22:55:24 -0300

    fix: keep legacy loading imports on unified indicator

---
+++ src/components/LoadingComponent.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ProcessingIndicatorComponent from "./ProcessingIndicatorComponent";
+
+export default function LoadingComponent() {
+  return <ProcessingIndicatorComponent messages={["Carregando a Plat…"]} />;
+}
