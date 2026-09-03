dbe1285f698fbd96b48b41853e8f7391c1483a86
Peter Tecnet <petertecnet@gmail.com>
2026-09-03 14:25:54 -0300

    feat(auth): enable password field enhancer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { startTelemetry } from "./telemetry";
 import { apiBaseUrl, appId, appSlug } from "./config";
 import { installGlobalImageFallbacks } from "./utils/imageFallback";
+import { installPasswordFieldEnhancer } from "./utils/passwordFieldEnhancer";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import axios from "axios";
@@ -19,6 +20,7 @@ import "./Responsive.css";
 import "./ProductionReady.css";
 
 installGlobalImageFallbacks();
+installPasswordFieldEnhancer();
 startTelemetry({ apiBaseUrl, appSlug, appId });
 
 axios.defaults.headers.common["X-Peter-App"] = appSlug;
