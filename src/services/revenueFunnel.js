b3b7a934c1e5808c828415037f48bd7e8d036e39
Peter Tecnet <petertecnet@gmail.com>
2026-09-15 17:48:54 -0300

    feat(revenue): measure PIX recovery funnel

--- src/services/revenueFunnel.js
+++ src/services/revenueFunnel.js
@@ -15,7 +15,13 @@ export const PIX_FUNNEL_EVENTS = [
   "frontend_payment_approved",
 ];
 
-const REVENUE_FUNNEL_EVENTS = [...ORDERING_FUNNEL_EVENTS, ...PIX_FUNNEL_EVENTS];
+export const PIX_RECOVERY_FUNNEL_EVENTS = [
+  "frontend_plat_guest_pix_recovery_presented",
+  "frontend_plat_guest_pix_recovery_copied",
+  "frontend_plat_guest_pix_recovery_paid",
+];
+
+const REVENUE_FUNNEL_EVENTS = [...ORDERING_FUNNEL_EVENTS, ...PIX_FUNNEL_EVENTS, ...PIX_RECOVERY_FUNNEL_EVENTS];
 const headers = () => ({ Authorization: `Bearer ${localStorage.getItem("token") || ""}` });
 
 export const getEstablishmentRevenueFunnel = async (establishmentId, days = 30) => {
